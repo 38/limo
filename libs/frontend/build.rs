61126d7473ad854add02a68b72ce5e6131458dbb
Hao Hou <haohou302@gmail.com>
2019-01-14 12:11:46 -0700

    Make sure limo uses the customized version of libhts

--- libs/frontend/build.rs
+++ libs/frontend/build.rs
@@ -20,23 +20,27 @@ fn create_hts_bindings(base:&str) -> Result<(), ()>
             .write_to_file("generated/htslib.rs")
             .expect("Unable to write the generated file");
     }
+    if !Path::new(&format!("{}/htslib/libhts-static.a", base)).exists() {
+        std::os::unix::fs::symlink(format!("{}/htslib/libhts.a", base), format!("{}/htslib/libhts-static.a", base))
+        .expect("Unable to create the symlink to libhts.a");
+    }
     Ok(())
 }
 fn main() -> Result<(), std::io::Error>
 {
-    let base = format!("{}/../", env::current_dir()?.as_path().to_str().unwrap());
+    let base = format!("{}/../", env::var("CARGO_MANIFEST_DIR").unwrap());
     let hts_bin_path = format!("{}/htslib/libhts.a", base);
     if let Err(_) = create_hts_bindings(base.as_str()) { return Err(std::io::Error::new(std::io::ErrorKind::Other, "Bindgen failed")); }
     if !Path::new(hts_bin_path.as_str()).exists()
     {
         Command::new("make")
             .arg(format!("-j{}", get_num_cpus()))
-            .current_dir("htslib")
+            .current_dir(format!("{}/htslib", base))
             .spawn()
             .expect("Unable to call makefile for htslib");
     }
     println!("cargo:rustc-link-search={}/htslib/", base);
-    println!("cargo:rustc-link-lib=hts");
+    println!("cargo:rustc-link-lib=hts-static");
     println!("cargo:rustc-link-lib=z");
     println!("cargo:rustc-link-lib=m");
     println!("cargo:rustc-link-lib=bz2");
